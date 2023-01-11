f367861c56e4924dd386f3b8abcbd71d92259fa2
Gabriel Lellouche <gabyk800@gmail.com>
2023-01-11 18:26:35 +0200

    transitioned backend to render

--- client/src/app/config/constants.ts
+++ client/src/app/config/constants.ts
@@ -1,8 +1,8 @@
 export const Constants = {
-  API_ENDPOINT: 'https://firemart-docker.herokuapp.com/',
-  API_USER_ENDPOINT: 'https://firemart-docker.herokuapp.com/users/',
-  API_PRODUCT_ENDPOINT: 'https://firemart-docker.herokuapp.com/products/',
-  API_CART_ENDPOINT: 'https://firemart-docker.herokuapp.com/cart/',
-  API_ORDER_ENDPOINT: 'https://firemart-docker.herokuapp.com/order/',
-  API_GALLERY_ENDPOINT: 'https://firemart-docker.herokuapp.com/gallery/',
+  API_ENDPOINT: 'https://firemart-backend.onrender.com/',
+  API_USER_ENDPOINT: 'https://firemart-backend.onrender.com/users/',
+  API_PRODUCT_ENDPOINT: 'https://firemart-backend.onrender.com/products/',
+  API_CART_ENDPOINT: 'https://firemart-backend.onrender.com/cart/',
+  API_ORDER_ENDPOINT: 'https://firemart-backend.onrender.com/order/',
+  API_GALLERY_ENDPOINT: 'https://firemart-backend.onrender.com/gallery/',
 };
